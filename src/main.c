23f9f0236d7c761d8ad9f69e3bdadbbe494161bd
Lucas Blotta <lucas@blotta.info>
2026-04-02 12:38:15 -0300

    extracted renderer

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "SDL3/SDL_init.h"
 #include <assert.h>
 #include <glad/glad.h>
 #include <stddef.h>
@@ -45,42 +46,116 @@ typedef struct
 } Vertex;
 
 #define VERTEX_BUF_CAP 1024
+typedef struct
+{
+    GLuint shader_program;
+    GLuint VAO;
+    GLuint VBO;
+    Vertex vertex_buf[VERTEX_BUF_CAP];
+    size_t vertex_buf_sz;
+} Renderer;
+
+int renderer_init(Renderer* r)
+{
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+    GLint vertex_compiled;
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &vertex_compiled);
+    if (vertex_compiled != GL_TRUE)
+    {
+        GLsizei log_length = 0;
+        GLchar message[1024];
+        glGetShaderInfoLog(vertex_shader, 1024, &log_length, message);
+        SDL_Log("Error compiling vertex shader: %s", message);
+        return -1;
+    }
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+    GLint fragment_compiled;
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &fragment_compiled);
+    if (fragment_compiled != GL_TRUE)
+    {
+        GLsizei log_length = 0;
+        GLchar message[1024];
+        glGetShaderInfoLog(fragment_shader, 1024, &log_length, message);
+        SDL_Log("Error compiling fragment shader: %s", message);
+        return -1;
+    }
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    GLint shader_program_linked;
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &shader_program_linked);
+    if (!shader_program_linked)
+    {
+        GLsizei log_length = 0;
+        GLchar message[1024];
+        glGetProgramInfoLog(shader_program, 1024, &log_length, message);
+        SDL_Log("Error linking shader program: %s", message);
+        return -1;
+    }
+    r->shader_program = shader_program;
+
+    r->vertex_buf_sz = 0;
+
+    glGenBuffers(1, &r->VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, r->VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(r->vertex_buf), r->vertex_buf, GL_DYNAMIC_DRAW);
+
+    glGenVertexArrays(1, &r->VAO);
+    glBindVertexArray(r->VAO);
+
+    glVertexAttribPointer(VA_POS, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, pos));
+    glEnableVertexAttribArray(VA_POS);
+
+    glVertexAttribPointer(VA_UV, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, uv));
+    glEnableVertexAttribArray(VA_UV);
+
+    glVertexAttribPointer(VA_COLOR, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
+    glEnableVertexAttribArray(VA_COLOR);
+
+    return 1;
+}
+
+void renderer_push_vertex(Renderer* r, Vec3 pos, Vec2 uv, Vec4 color)
+{
+    assert(r->vertex_buf_sz < VERTEX_BUF_CAP);
+    r->vertex_buf[r->vertex_buf_sz].pos = pos;
+    r->vertex_buf[r->vertex_buf_sz].uv = uv;
+    r->vertex_buf[r->vertex_buf_sz].color = color;
+    r->vertex_buf_sz += 1;
+}
+
+void renderer_push_rect(Renderer* r, SDL_FRect rect)
+{
+    Vec4 color = vec4(1.f, 1.f, 1.f, 1.f);
+    renderer_push_vertex(r, vec3(rect.x, rect.y, 0), vec2(0.f, 0.f), color);          // a
+    renderer_push_vertex(r, vec3(rect.x + rect.w, rect.y, 0), vec2(1.f, 0.f), color); // b
+    renderer_push_vertex(r, vec3(rect.x, rect.y + rect.h, 0), vec2(0.f, 1.f), color); // c
+
+    renderer_push_vertex(r, vec3(rect.x, rect.y + rect.h, 0), vec2(0.f, 1.f), color);          // c
+    renderer_push_vertex(r, vec3(rect.x + rect.w, rect.y, 0), vec2(1.f, 0.f), color);          // b
+    renderer_push_vertex(r, vec3(rect.x + rect.w, rect.y + rect.h, 0), vec2(1.f, 1.f), color); // d
+}
+
+void renderer_sync_buf(Renderer* r)
+{
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Vertex) * r->vertex_buf_sz, r->vertex_buf);
+}
 
 typedef struct
 {
     SDL_Window* window;
     SDL_GLContext gl_context;
-    GLuint shader_program;
-    GLuint VAO;
-    Vertex vertex_buf[VERTEX_BUF_CAP];
-    size_t vertex_buf_sz;
+    Renderer renderer;
 } AppState;
-
-void vertex_buf_push(AppState* state, Vec3 pos, Vec2 uv, Vec4 color)
-{
-    assert(state->vertex_buf_sz < VERTEX_BUF_CAP);
-    state->vertex_buf[state->vertex_buf_sz].pos = pos;
-    state->vertex_buf[state->vertex_buf_sz].uv = uv;
-    state->vertex_buf[state->vertex_buf_sz].color = color;
-    state->vertex_buf_sz += 1;
-}
-
-void vertex_buf_push_rect(AppState* state, SDL_FRect rect)
-{
-    Vec4 color = vec4(1.f, 1.f, 1.f, 1.f);
-    vertex_buf_push(state, vec3(rect.x, rect.y, 0), vec2(0.f, 0.f), color); // a
-    vertex_buf_push(state, vec3(rect.x + rect.w, rect.y, 0), vec2(1.f, 0.f), color); // b
-    vertex_buf_push(state, vec3(rect.x, rect.y + rect.h, 0), vec2(0.f, 1.f), color); // c
-
-    vertex_buf_push(state, vec3(rect.x, rect.y + rect.h, 0), vec2(0.f, 1.f), color); // c
-    vertex_buf_push(state, vec3(rect.x + rect.w, rect.y, 0), vec2(1.f, 0.f), color); // b
-    vertex_buf_push(state, vec3(rect.x + rect.w, rect.y + rect.h, 0), vec2(1.f, 1.f), color); // d
-}
-
-void vertex_buf_sync(AppState* state)
-{
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Vertex) * state->vertex_buf_sz, state->vertex_buf);
-}
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
@@ -132,79 +207,15 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
     SDL_GetWindowSizeInPixels(window, &w, &h);
     glViewport(0, 0, w, h);
 
-    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
-    glCompileShader(vertex_shader);
-    GLint vertex_compiled;
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &vertex_compiled);
-    if (vertex_compiled != GL_TRUE)
-    {
-        GLsizei log_length = 0;
-        GLchar message[1024];
-        glGetShaderInfoLog(vertex_shader, 1024, &log_length, message);
-        SDL_Log("Error compiling vertex shader: %s", message);
+    if (!renderer_init(&state->renderer)) {
         return SDL_APP_FAILURE;
     }
 
-    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
-    glCompileShader(fragment_shader);
-    GLint fragment_compiled;
-    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &fragment_compiled);
-    if (fragment_compiled != GL_TRUE)
-    {
-        GLsizei log_length = 0;
-        GLchar message[1024];
-        glGetShaderInfoLog(fragment_shader, 1024, &log_length, message);
-        SDL_Log("Error compiling fragment shader: %s", message);
-        return SDL_APP_FAILURE;
-    }
-
-    GLuint shader_program = glCreateProgram();
-    glAttachShader(shader_program, vertex_shader);
-    glAttachShader(shader_program, fragment_shader);
-    glLinkProgram(shader_program);
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
-    GLint shader_program_linked;
-    glGetProgramiv(shader_program, GL_LINK_STATUS, &shader_program_linked);
-    if (!shader_program_linked)
-    {
-        GLsizei log_length = 0;
-        GLchar message[1024];
-        glGetProgramInfoLog(shader_program, 1024, &log_length, message);
-        SDL_Log("Error linking shader program: %s", message);
-        return SDL_APP_FAILURE;
-    }
-    state->shader_program = shader_program;
-
-    state->vertex_buf_sz = 0;
-
-    GLuint VBO, VAO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(state->vertex_buf), state->vertex_buf, GL_DYNAMIC_DRAW);
-
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-    state->VAO = VAO;
-
-    glVertexAttribPointer(VA_POS, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, pos));
-    glEnableVertexAttribArray(VA_POS);
-
-    glVertexAttribPointer(VA_UV, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, uv));
-    glEnableVertexAttribArray(VA_UV);
-
-    glVertexAttribPointer(VA_COLOR, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
-    glEnableVertexAttribArray(VA_COLOR);
-
-    vertex_buf_push_rect(state, (SDL_FRect){-0.5f, -0.5f, 1.f, 1.f});
-    vertex_buf_push_rect(state, (SDL_FRect){-0.25f, -0.25f, 1.f, 1.f});
-    vertex_buf_sync(state);
+    renderer_push_rect(&state->renderer, (SDL_FRect){-0.5f, -0.5f, 1.f, 1.f});
+    renderer_push_rect(&state->renderer, (SDL_FRect){-0.25f, -0.25f, 1.f, 1.f});
+    renderer_sync_buf(&state->renderer);
 
     *appstate = state;
-
-    SDL_Log("vertex_buf_sz: %d", (int)state->vertex_buf_sz);
 
     return SDL_APP_CONTINUE;
 }
@@ -229,7 +240,6 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
     }
     }
 
-
     return SDL_APP_CONTINUE;
 }
 
@@ -241,11 +251,11 @@ SDL_AppResult SDL_AppIterate(void* appstate)
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glUseProgram(state->shader_program);
-    glBindVertexArray(state->VAO);
+    glUseProgram(state->renderer.shader_program);
+    glBindVertexArray(state->renderer.VAO);
 
     // glDrawArrays(GL_TRIANGLES, 0, 4);
-    glDrawArraysInstanced(GL_TRIANGLES, 0, state->vertex_buf_sz, 1);
+    glDrawArraysInstanced(GL_TRIANGLES, 0, state->renderer.vertex_buf_sz, 1);
 
     SDL_GL_SwapWindow(state->window);
 
@@ -261,8 +271,8 @@ void SDL_AppQuit(void* appstate, SDL_AppResult result)
         AppState* state = (AppState*)appstate;
         SDL_GL_DestroyContext(state->gl_context);
         SDL_DestroyWindow(state->window);
-        glDeleteVertexArrays(1, &state->VAO);
-        glDeleteProgram(state->shader_program);
+        glDeleteVertexArrays(1, &state->renderer.VAO);
+        glDeleteProgram(state->renderer.shader_program);
         SDL_free(state);
     }
     SDL_Quit();
